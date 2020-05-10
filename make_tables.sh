cc64e6696a148886747eb9b455e8d3796cba3ebf
Andrew Sherman <asherman@cloudera.com>
2020-05-10 08:58:17 -0700

    drop-create

--- make_tables.sh
+++ make_tables.sh
@@ -1,6 +1,13 @@
-STR1=$(head -1 Assessor_Historical_Secured_Property_Tax_Rolls.csv | sed 's/ /_/g' | sed 's/-/_/g')
+PROPERTY=Assessor_Historical_Secured_Property_Tax_Rolls.csv
+STR1=$(head -1 $PROPERTY | sed 's/ /_/g' | sed 's/-/_/g')
 
 CREATE="create table property ( $(echo $STR1 , | sed 's/,/ string,/g') dummy int ) row format delimited fields terminated by \",\";"
 
+impala-shell.sh -q "drop table if exists property"
 impala-shell.sh -q "$CREATE"
+
+
+hdfs dfs -ls /test-warehouse/property
+# hdfs dfs -put /test-warehouse/property 
+
 
