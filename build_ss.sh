2ee8448c23716d857301e64ba488435846c34a5b
Andrew Sherman <asherman@cloudera.com>
2020-05-10 12:53:12 -0700

    strip some 0s

--- build_ss.sh
+++ build_ss.sh
@@ -3,5 +3,7 @@ OUT=6_stories.csv
 TMP=6_stories.raw
 impala-shell.sh  -B < 6_stories.sql -o $TMP
 echo "Address,City" > $OUT
-cat $TMP | cut -c 6- | cut -c -25 | sed 's/$/,San Francisco/' >> $OUT
+cat $TMP | cut -c 6- | cut -c -25 |\
+    awk '{street=$2; gsub ("^0*", "", street); print $1, street}' |\
+    sed 's/$/,San Francisco/' >> $OUT
 
