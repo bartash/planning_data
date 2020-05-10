4ce50f44d585f60dbf7ff9e60fb443258b92e6e3
Andrew Sherman <asherman@cloudera.com>
2020-05-10 10:45:44 -0700

    start inner join

--- queries.sh
+++ queries.sh
@@ -13,6 +13,10 @@ select count(property_location),assessor_neighborhood from property
 where cast(number_of_stories as tinyint) = 7 
 group by assessor_neighborhood order by count(property_location) desc; 
 
+select count(property_location),assessor_neighborhood from property 
+where cast(number_of_stories as tinyint) = 6 
+group by assessor_neighborhood order by count(property_location) desc; 
+
 
 # 555 California
 select property_location,number_of_stories  from property where cast(parcel_number as int)  = 0259027;
@@ -23,5 +27,14 @@ select property_location,number_of_stories  from property where cast(parcel_numb
 
 # 302 silver
 select property_location,number_of_stories  from property where property_location like '%302 SILVER%';
+
+select max(closed_roll_year), parcel_number  from property group by parcel_number
+
+# start inner join
+select property_location,number_of_stories  from property p  inner join (
+select max(closed_roll_year) as year, parcel_number  from property group by parcel_number) sub on p.closed_roll_year = sub.year
+and p.parcel_number = sub.parcel_number;
+
+
 
 
