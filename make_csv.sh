26f69fc7733416c3529d0dd563669c10d1623dc5
Andrew Sherman <asherman@cloudera.com>
2020-05-10 15:50:31 -0700

    add ability to remove addresses

--- make_csv.sh
+++ make_csv.sh
@@ -1,9 +1,11 @@
 OUT=$1.csv
 SQL=$1.sql
 TMP=$1.raw
+REM=$1.rem
 impala-shell.sh  -B < $SQL -o $TMP
 echo "Address,City" > $OUT
 cat $TMP | tr -d \" | cut -c 6- | cut -c -27 | sort -u |\
     awk '{street=$2; addr=$1; gsub ("^0*", "", addr); gsub ("^0*", "", street); print addr, street, $3, $4, $5 }' |\
-    sed 's/$/,San Francisco/' | sed 's/ TE *,/ TERRACE ,/' >> $OUT
+    sed 's/$/,San Francisco/' | sed 's/ TE *,/ TERRACE ,/' |\
+    grep -v -f $REM >> $OUT
 
