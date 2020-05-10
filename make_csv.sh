f41e4a716e3c7a62e9be107cc488673abcacb5ae
Andrew Sherman <asherman@cloudera.com>
2020-05-10 15:02:59 -0700

    rm confusing double quotes

--- make_csv.sh
+++ make_csv.sh
@@ -3,7 +3,7 @@ SQL=$2
 TMP=$3
 impala-shell.sh  -B < $SQL -o $TMP
 echo "Address,City" > $OUT
-cat $TMP | cut -c 6- | cut -c -25 | sort -u |\
+cat $TMP | tr -d \" | cut -c 6- | cut -c -25 | sort -u |\
     awk '{street=$2; addr=$1; gsub ("^0*", "", addr); gsub ("^0*", "", street); print addr, street, $3, $4, $5 }' |\
     sed 's/$/,San Francisco/' >> $OUT
 
