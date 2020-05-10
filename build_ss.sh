d20c3d1e6085c214ebba32ea0870984cb0b6b546
Andrew Sherman <asherman@cloudera.com>
2020-05-10 14:09:17 -0700

    strip dups

--- build_ss.sh
+++ build_ss.sh
@@ -3,7 +3,7 @@ OUT=6_stories.csv
 TMP=6_stories.raw
 impala-shell.sh  -B < 6_stories.sql -o $TMP
 echo "Address,City" > $OUT
-cat $TMP | cut -c 6- | cut -c -25 |\
+cat $TMP | cut -c 6- | cut -c -25 | sort -u \
     awk '{street=$2; addr=$1; gsub ("^0*", "", addr); gsub ("^0*", "", street); print addr, street, $3 $4 $5 }' |\
     sed 's/$/,San Francisco/' >> $OUT
 
