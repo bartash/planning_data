886a0c786c772cf4f1c261ea4a862bb9aa4a39ef
Andrew Sherman <asherman@cloudera.com>
2020-05-10 09:01:11 -0700

    read count of property

--- make_tables.sh
+++ make_tables.sh
@@ -1,3 +1,6 @@
+# create date
+# https://docs.cloudera.com/documentation/enterprise/6/6.3/topics/impala_txtfile.html
+
 PROPERTY=Assessor_Historical_Secured_Property_Tax_Rolls.csv
 STR1=$(head -1 $PROPERTY | sed 's/ /_/g' | sed 's/-/_/g')
 
@@ -7,7 +10,9 @@ impala-shell.sh -q "drop table if exists property"
 impala-shell.sh -q "$CREATE"
 
 
+hdfs dfs -put $PROPERTY /test-warehouse/property 
 hdfs dfs -ls /test-warehouse/property
-# hdfs dfs -put /test-warehouse/property 
 
 
+impala-shell.sh -q "refresh property"
+impala-shell.sh -q "select count(*) from property"
