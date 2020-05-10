7ce055f383672fb82b75ffa6e57385b5b47a830d
Andrew Sherman <asherman@cloudera.com>
2020-05-10 11:36:15 -0700

    start massaging output

--- build_ss.sh
+++ build_ss.sh
@@ -1,2 +1,3 @@
 # buid the csv files
 impala-shell.sh  -B < 6_stories.sql -o 6_stories.raw
+cat 6_stories.raw | cut -c 6- | cut -c -25 | sed 's/$/,San Francisco/' 
--- queries.sh
+++ queries.sh
@@ -59,6 +59,13 @@ cast(p.number_of_stories as tinyint) > 5  and
 cast(p.number_of_stories as tinyint) < 8;
 
 
+# 6 storey buildings
+select property_location  from property p  inner join (
+select max(closed_roll_year) as year, parcel_number  from property group by parcel_number) sub on p.closed_roll_year = sub.year
+and p.parcel_number = sub.parcel_number and 
+cast(p.number_of_stories as tinyint) = 6;
+
+
 
 
 
