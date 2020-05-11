d63627f34c60b6c4020fe5b7636ad96ed53c6051
Andrew Sherman <asherman@cloudera.com>
2020-05-10 17:12:45 -0700

    use a simple query to generate the V4 maps

--- 4_5_stories.sql
+++ 4_5_stories.sql
@@ -1,5 +1,3 @@
-select property_location  from property p  inner join (
-select max(closed_roll_year) as year, parcel_number  from property group by parcel_number) sub on p.closed_roll_year = sub.year
-and p.parcel_number = sub.parcel_number and
-cast(p.number_of_stories as tinyint) >= 4 and
-cast(p.number_of_stories as tinyint) <= 5;
+select property_location,number_of_stories  from property where cast(number_of_stories as tinyint) >= 4 and
+cast(number_of_stories as tinyint) <= 5;
+
--- 7_8_stories.sql
+++ 7_8_stories.sql
@@ -1,5 +1,2 @@
-select property_location  from property p  inner join (
-select max(closed_roll_year) as year, parcel_number  from property group by parcel_number) sub on p.closed_roll_year = sub.year
-and p.parcel_number = sub.parcel_number and
-cast(p.number_of_stories as tinyint) >= 7 and
-cast(p.number_of_stories as tinyint) <= 8;
+select property_location,number_of_stories  from property where cast(number_of_stories as tinyint) >= 7 and
+cast(number_of_stories as tinyint) <= 8;
